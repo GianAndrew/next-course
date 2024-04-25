0a69f694774f1cf2a9b1074eead6e723821fea0a
Gian Del Rosario <giandrew03@gmail.com>
2024-04-25 14:43:06 +0800

    added middleware

--- src/app/api/product/data.ts
+++ src/app/api/product/data.ts
@@ -1,8 +1,8 @@
 export const products = [
 	{ id: 1, name: 'keyboard', price: 1000, stock: 50 },
 	{ id: 2, name: 'mouse', price: 500, stock: 100 },
-	{ id: 3, name: 'monitor', price: 1500, stock: 10 },
-	{ id: 4, name: 'cpu', price: 2000, stock: 5 },
+	{ id: 3, name: 'monitor', price: 1000, stock: 10 },
+	{ id: 4, name: 'cpu', price: 2500, stock: 5 },
 	{ id: 5, name: 'gpu', price: 2500, stock: 15 },
-	{ id: 6, name: 'ram', price: 3000, stock: 20 },
+	{ id: 6, name: 'ram', price: 1000, stock: 10 },
 ];
--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,10 +1,27 @@
+import { type NextRequest } from 'next/server';
 import { products } from './data';
+import { cookies, headers } from 'next/headers';
 
-export const GET = async () => {
-	return Response.json({ products });
+export const GET = async (request: NextRequest) => {
+	const { searchParams } = request.nextUrl;
+
+	const requestHeaders = new Headers(request.headers);
+	const headerList = headers();
+
+	const price = searchParams.get('price');
+	const stock = searchParams.get('stock');
+
+	console.log(requestHeaders.get('Authorization'));
+	console.log(headerList.get('Authorization'));
+
+	const filteredProducts = price || stock ? products.filter((product) => product.price === parseInt(price!) && product.stock === parseInt(stock!)) : products;
+	return Response.json({ products: filteredProducts });
 };
 
 export const POST = async (request: Request) => {
 	const body = await request.json();
+
+	cookies().set('access_token', '123ikesubgfiu_iku3h457skjfihuwhjghian-da0934kl', { httpOnly: true, secure: true });
+
 	return Response.json(body);
 };
